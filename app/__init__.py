9ffa98905606520a5c8d8e6ede2b17c7f13917a4
UMUTONIRitha <rithamarie9@gmail.com>
2020-12-04 08:54:38 +0200

    file initialization

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from config import config_options
+
+def create_app(configuration):
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[configuration])
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    return app
---
+++ config.py
@@ -0,0 +1,15 @@
+class Config:
+    pass
+ 
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
