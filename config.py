20ba882ebb11e56ab6aa7e13b24b206767876f9d
UMUTONIRitha <rithamarie9@gmail.com>
2020-12-05 20:43:32 +0200

    add config

--- config.py
+++ config.py
@@ -1,15 +1,33 @@
-class Config:
-    pass
- 
-class ProdConfig(Config):
-    pass
+import os
 
+class Config:
+
+    QUOTES_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:12345@localhost/unique_blog'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+class ProdConfig(Config):
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:12345@localhost/unique_blog_test'
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:12345@localhost/unique_blog'
+
     DEBUG = True
-
-
+    
 config_options = {
 'development':DevConfig,
-'production':ProdConfig
+'production':ProdConfig,
+'test':TestConfig
 }
