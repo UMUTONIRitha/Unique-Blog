bbb52bb6660a001aa26382710cf56d9af1d5ef16
UMUTONIRitha <rithamarie9@gmail.com>
2020-12-06 20:09:26 +0200

    add changes

--- app/email.py
+++ app/email.py
@@ -2,7 +2,7 @@ from flask_mail import Message
 from flask import render_template
 from . import mail
 def mail_message(subject,template,to,**kwargs):
-    sender_email = 'rithamarie9@gmail.com'
+    sender_email = 'umutonir6@gmail.com'
     email = Message(subject, sender=sender_email, recipients=[to])
     email.body= render_template(template + ".txt",**kwargs)
     email.html = render_template(template + ".html",**kwargs)
