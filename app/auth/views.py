038b0e9adeef49954d5bc7a293d449add4964ae1
UMUTONIRitha <rithamarie9@gmail.com>
2020-12-06 20:16:49 +0200

    save changes

--- app/auth/views.py
+++ app/auth/views.py
@@ -30,7 +30,7 @@ def register():
     if form.validate_on_submit():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         user.save_user()
-        # mail_message('Welcome to my personal blog','email/welcome_user',user.email,user = user)
+        mail_message('Welcome to my personal blog','email/welcome_user',user.email,user = user)
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
